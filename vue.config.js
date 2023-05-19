7db403ea19f03247a4c1107fc16766238322e587
ray <ray.lee@osckorea.com>
2023-05-19 16:14:41 +0900

    V 1.0.0

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  outputDir: path.resolve(__dirname, "../demo-api/src/main/resources/static"),
+  indexPath: path.resolve(
+    __dirname,
+    "../demo-api/src/main/resources/static/index.html"
+  ),
+
+  devServer: {
+    proxy: {
+      "/": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
 });
